1042eeaaf863f0521d14d65aa9712610481e2cbb
Marek Valko <valko.marek@gmail.com>
2020-03-05 21:36:00 +0100

    Basic styling for organisations

--- interview-project/src/components/OrganisationItem.tsx
+++ interview-project/src/components/OrganisationItem.tsx
@@ -1,15 +1,20 @@
-import React, { FunctionComponent } from 'react'
-import Organisation from '../types/Organisation';
+import React, { FunctionComponent } from "react";
+import Organisation from "../types/Organisation";
+import { Card, Box } from "@material-ui/core";
 
-const OrganisationItem: FunctionComponent<{organisation: Organisation}> = ({organisation}) => {
-    return (
-        <div>
-
-            <h1>OrganisationItem</h1>
-            {organisation.login}
-        </div>
-
-    )
-}
+const OrganisationItem: FunctionComponent<{ organisation: Organisation }> = ({
+  organisation
+}) => {
+  return (
+    <Box my={1}>
+      <Card>
+        <Box p={1}>
+          <h1>OrganisationItem</h1>
+          {organisation.login}
+        </Box>
+      </Card>
+    </Box>
+  );
+};
 
 export default OrganisationItem;
--- interview-project/src/components/OrganisationList.tsx
+++ interview-project/src/components/OrganisationList.tsx
@@ -1,18 +1,23 @@
-import React, {FunctionComponent} from 'react'
-import Organisation from '../types/Organisation';
-import OrganisationItem from './OrganisationItem';
+import React, { FunctionComponent } from "react";
+import Organisation from "../types/Organisation";
+import OrganisationItem from "./OrganisationItem";
+import { Grid, Typography, Box } from "@material-ui/core";
 
-const OrganisationList:FunctionComponent<{organisations: Organisation[]}> = ({organisations}) => {
-    return (
-        <div className="organisationList">
-            <h1>Organisations</h1>
-            {organisations.map((org: Organisation) =>
-            <OrganisationItem key={org.login} organisation={org}/>
-            )}
-
-        </div>
-    )
-
-}
+const OrganisationList: FunctionComponent<{
+  organisations: Organisation[];
+}> = ({ organisations }) => {
+  return (
+    <Box m={1}>
+      <Grid direction="column">
+        <Typography variant="h3" align="center">
+          Organisations
+        </Typography>
+        {organisations.map((org: Organisation) => (
+          <OrganisationItem key={org.login} organisation={org} />
+        ))}
+      </Grid>
+    </Box>
+  );
+};
 
 export default OrganisationList;
