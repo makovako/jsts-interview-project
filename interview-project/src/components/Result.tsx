f58c076480ef6df2bb217c462d262a4289b4a7b6
Marek Valko <valko.marek@gmail.com>
2020-03-05 21:35:43 +0100

    Styled main result part with MUI

--- interview-project/src/components/Result.tsx
+++ interview-project/src/components/Result.tsx
@@ -4,21 +4,53 @@ import RepositoryList from "./RepositoryList";
 import Organisation from "../types/Organisation";
 import Repository from "../types/Repository";
 import User from "../types/User";
+import {
+  Box,
+  Typography,
+  Card,
+  Grid,
+  Link,
+} from "@material-ui/core";
 
 const Result: FunctionComponent<{
   organisations: Organisation[];
   repositories: Repository[];
   user: User | undefined;
 }> = ({ organisations, repositories, user }) => {
-  return (
-    <div className="results">
-      <h1>Results</h1>
-      <div className="panes">
-        {user && <p>{user.login}</p>}
-        <RepositoryList repositories={repositories} />
-        <OrganisationList organisations={organisations} />
-      </div>
-    </div>
+  return user ? (
+    <Box>
+      <Grid container>
+        <Grid item xs={8}>
+          <Card>
+            <Link href={user.url} underline="none">
+              <Box p={1} display="flex" justifyContent="flex-start">
+                <Box mx={2}>
+                  <img src={user.avatar_url} alt="Avatar" height="50" />
+                </Box>
+                <Box mx={2} display="flex" flexDirection="column">
+                  <Typography color="textPrimary">
+                    Username: {user?.login}
+                  </Typography>
+                  <Typography color="textPrimary">
+                    Name: {user?.name}
+                  </Typography>
+                </Box>
+              </Box>
+            </Link>
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <RepositoryList repositories={repositories} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <OrganisationList organisations={organisations} />
+        </Grid>
+      </Grid>
+    </Box>
+  ) : (
+    <Box></Box>
   );
 };
 
