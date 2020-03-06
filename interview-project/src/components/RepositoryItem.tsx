ce9a986ceac65d5c7d5596c08d366854b39f5d58
Marek Valko <valko.marek@gmail.com>
2020-03-06 11:34:34 +0100

    Designed repository item

--- interview-project/src/components/RepositoryItem.tsx
+++ interview-project/src/components/RepositoryItem.tsx
@@ -1,17 +1,49 @@
 import React, { FunctionComponent } from "react";
 import Repository from "../types/Repository";
-import { Card, Box } from "@material-ui/core";
+import { Card, Box, Grid, Link, Typography } from "@material-ui/core";
+import { Restaurant, ErrorOutline } from "@material-ui/icons";
 
 const RepositoryItem: FunctionComponent<{ repository: Repository }> = ({
   repository
 }) => {
   return (
     <Box my={1}>
-      <Card>
-        <Box p={1}>
-          <h1>RepositoryItem</h1>
-          {repository.name}
-        </Box>
+      <Card raised>
+        <Link href={repository.url} underline="none">
+          <Box p={2}>
+            <Grid container direction="column">
+              <Grid item>
+                <Grid container justify="space-between">
+                  <Grid item xs={8}>
+                    <Typography color="textPrimary" variant="h4">
+                      {repository.name}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Grid container justify="center" alignItems="center">
+                      <Typography color="textPrimary">
+                        <ErrorOutline /> {repository.open_issues_count}
+                      </Typography>
+                      <Typography color="textPrimary">
+                        <Restaurant /> {repository.forks_count}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Typography color="textSecondary">
+                  Created: {new Date(repository.created_at).toDateString()}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography color="textPrimary">
+                  {repository.description}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </Link>
       </Card>
     </Box>
   );
--- interview-project/src/components/RepositoryList.tsx
+++ interview-project/src/components/RepositoryList.tsx
@@ -3,24 +3,27 @@ import Repository from "../types/Repository";
 import { Box, Grid, Typography } from "@material-ui/core";
 import RepositoryItem from "./RepositoryItem";
 
-
 const RepositoryList: FunctionComponent<{
   repositories: Repository[];
-}> = ({repositories}) => {
-
+}> = ({ repositories }) => {
   return (
-      <Box m={1}>
-      <Grid direction="column">
-        <Typography variant="h3" align="center">
-          Repositories
-        </Typography>
-        {repositories.map((repo: Repository) => (
-          <RepositoryItem key={repo.name} repository={repo} />
-        ))}
+    <Box my={1}>
+      <Grid container direction="column">
+        <Grid item>
+          <Box p={2}>
+            <Typography variant="h3" align="center">
+              Repositories
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item>
+          {repositories.map((repo: Repository) => (
+            <RepositoryItem key={repo.name} repository={repo} />
+          ))}
+        </Grid>
       </Grid>
     </Box>
-      
-  )
+  );
 };
 
 export default RepositoryList;
