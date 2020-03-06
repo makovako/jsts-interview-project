3f6b0ee23047f04f6088ac5007236cf505899df9
Marek Valko <valko.marek@gmail.com>
2020-03-06 11:54:23 +0100

    Updates

---
+++ precache-manifest.e86b2d8ed05737ed8d50b3e5839404ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797d3f92b5c95ce6181750f2152a3beb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "520ad7264d15bcee9bfc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b4b63a7afc56484d87bd",
+    "url": "/static/js/2.516aada7.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.516aada7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520ad7264d15bcee9bfc",
+    "url": "/static/js/main.d72f0e87.chunk.js"
+  },
+  {
+    "revision": "ff9be91d53064840151a",
+    "url": "/static/js/runtime-main.783ffd18.js"
+  }
+]);
